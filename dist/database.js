d207c87036c66326c573330881b39d479ceb86d0
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-01-30 18:52:47 +0100

    adjusted database.ts

--- dist/database.js
+++ dist/database.js
@@ -7,16 +7,18 @@ var pg_1 = require("pg");
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1["default"].config();
 var _a = process.env, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_TEST_DB = _a.POSTGRES_TEST_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, POSTGRES_HOST = _a.POSTGRES_HOST, ENV = _a.ENV;
+console.log(ENV);
+var db = '';
+if (ENV === 'test') {
+    db = POSTGRES_TEST_DB;
+}
+if (ENV === 'dev') {
+    db = POSTGRES_DB;
+}
 var client = new pg_1.Pool({
     host: POSTGRES_HOST,
     user: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
-    database: POSTGRES_DB
+    database: db
 });
-// const client = new Pool({
-//   host: POSTGRES_HOST,
-//   user: POSTGRES_USER,
-//   password: POSTGRES_PASSWORD,
-//   database: POSTGRES_TEST_DB,
-// });
 exports["default"] = client;
--- src/database.ts
+++ src/database.ts
@@ -12,18 +12,19 @@ const {
   ENV,
 } = process.env;
 
+console.log(ENV);
+var db: string = '';
+ENV === 'test'
+  ? (db = POSTGRES_TEST_DB as string)
+  : ENV === 'dev'
+  ? (db = POSTGRES_DB as string)
+  : '';
+
 const client = new Pool({
   host: POSTGRES_HOST,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
-  database: POSTGRES_DB,
+  database: db,
 });
-
-// const client = new Pool({
-//   host: POSTGRES_HOST,
-//   user: POSTGRES_USER,
-//   password: POSTGRES_PASSWORD,
-//   database: POSTGRES_TEST_DB,
-// });
 
 export default client;

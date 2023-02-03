4a720e05c09c9d238ec19be449f6084e1375ef44
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-02-03 11:46:59 +0100

    added a dashboard service and route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import users from './api/users';
 import products from './api/products';
 import orders from './api/orders';
+import dashboard from './api/dashboard';
 
 const routes = express.Router();
 routes.use('/users', users);
 routes.use('/products', products);
 routes.use('/orders', orders);
+routes.use('/dashboard', dashboard)
 
 export default routes;

fd23fd0cdb547bba3bb9d28e44bcda3cca7b82f0
Dominion Ikpeawujor <dominion.ikpeawujor@stu.cu.edu.ng>
2023-02-03 10:26:29 +0100

    edited migration for orders products table

--- migrations/sqls/20230203092135-orders-products-table-down.sql
+++ migrations/sqls/20230203092135-orders-products-table-down.sql
@@ -1,1 +1,6 @@
-/* Replace with your SQL commands */
+CREATE TABLE orders_products(
+    id SERIAL PRIMARY KEY,
+    quantity integer,
+    orders_id  bigint REFERENCES orders(id),
+    product_id bigint REFERENCES products(id),
+)
